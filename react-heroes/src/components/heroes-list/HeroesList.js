2477ca001adb3bd9eef2eee2d65928db9004e36f
m.kikmunter <M.Kikmunter@gmail.com>
2021-12-04 21:16:11 +0000

    removed empty object in cmpnt

--- react-heroes/src/components/heroes-list/HeroesList.js
+++ react-heroes/src/components/heroes-list/HeroesList.js
@@ -6,24 +6,24 @@ import HeroCard from '../hero-card/HeroCard';
 import './HeroesList.css';
 
 const HeroesList = (props) => {
-	return (
-		<div className="container">
-			<div className="content">
-				<h1 className="content-title font-size-22">
-					Heroes{' '}
-					<button className="btn btn-sm btn-action" type="button">
-						<FontAwesomeIcon
-							icon={faPlus}
-							className="add-hero-btn"
-						/>
-					</button>
-				</h1>
-			</div>
-			{props.heroes.map((hero) => (
-				<HeroCard key={hero.id} hero={hero}></HeroCard>
-			))}
-		</div>
-	);
+    return (
+        <div className="container">
+            <div className="content">
+                <h1 className="content-title font-size-22">
+                    Heroes
+                    <button className="btn btn-sm btn-action" type="button">
+                        <FontAwesomeIcon
+                            icon={faPlus}
+                            className="add-hero-btn"
+                        />
+                    </button>
+                </h1>
+            </div>
+            {props.heroes.map((hero) => (
+                <HeroCard key={hero.id} hero={hero}></HeroCard>
+            ))}
+        </div>
+    );
 };
 
 export default HeroesList;
